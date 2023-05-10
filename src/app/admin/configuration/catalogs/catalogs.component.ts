9095a3ab2582b2c8c203292bf5ce574761ee1da8
Elmer Danilo Hernandez <eldh.dev@gmail.com>
2023-05-10 16:03:51 -0600

    update

--- src/app/admin/configuration/catalogs/catalogs.component.ts
+++ src/app/admin/configuration/catalogs/catalogs.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { CatalogService } from '../../core/services/catalog.service';
+
 @Component({
   selector: 'app-catalogs',
   templateUrl: './catalogs.component.html',
   styleUrls: ['./catalogs.component.scss']
 })
 export class CatalogsComponent {
+  title: string =  'Actividades Económicas';
 
 }
